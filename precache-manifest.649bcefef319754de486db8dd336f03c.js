c177d017831ebf54de551e9bcfd5ca6799de78c5
msyphus <mark.syphus@gmail.com>
2020-07-10 19:27:07 -0600

    Updates

---
+++ precache-manifest.649bcefef319754de486db8dd336f03c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96886ecbe5feef3e8c14b449eabeae96",
+    "url": "/fish-finder/index.html"
+  },
+  {
+    "revision": "1f8d0480046fd726bd45",
+    "url": "/fish-finder/static/css/main.d1ab0ee4.chunk.css"
+  },
+  {
+    "revision": "e55ed680437e28c06a1d",
+    "url": "/fish-finder/static/js/2.66198967.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fish-finder/static/js/2.66198967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8d0480046fd726bd45",
+    "url": "/fish-finder/static/js/main.cb5d14a7.chunk.js"
+  },
+  {
+    "revision": "795fa0a5d9c53d19797f",
+    "url": "/fish-finder/static/js/runtime-main.fae072c6.js"
+  }
+]);
